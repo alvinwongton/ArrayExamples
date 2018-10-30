0509ddc57efeb8af2e9ec7fd3c3c4557764c8767
alvinwongton <31286634+alvinwongton@users.noreply.github.com>
2018-10-30 11:21:16 -0700

    did things

---
+++ EnhancedFor.java
@@ -0,0 +1,13 @@
+public class EnhancedFor
+{
+   public static void main(String[] args)
+   {
+         int[] myInts = {2,3,5,7,11,13,17,19,21};
+         
+         for (int i : myInts)
+         {
+             System.out.println(i);
+         } 
+    }
+    
+}
---
+++ Factorial.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Factorial
+{
+    public static void main (String[] args)
+    {
+        Scanner s = new Scanner (System.in);
+        System.out.print("Type in a small integer.");
+        int n = s.nextInt();
+        int answer = factorial(n);
+        System.out.println("Answer is "+answer);
+    }
+    
+        public static int factorial (int n)
+       
+
+        {
+            System.out.println(n);
+            if(n>1)
+            return n*factorial(n-1);
+            
+            else
+     
+            return n;
+            
+            
+
+        }
+
+    }
